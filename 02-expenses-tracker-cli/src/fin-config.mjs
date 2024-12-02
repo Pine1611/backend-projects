29578a00a00fd07587a62ad115b6e9a99a4c3956
Pine1611 <iampine1611@gmail.com>
2024-12-02 09:54:49 +0700

    BE02 - expenses tracker cli

---
+++ 02-expenses-tracker-cli/src/fin-config.mjs
@@ -0,0 +1,13 @@
+import * as dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+// config data path
+const FOLDER_NAME = process.env.FOLDER_NAME ? process.env.FOLDER_NAME : process.cwd() + "\\data-cli\\";
+const FILE_NAME = process.env.FILE_NAME ? process.env.FILE_NAME : "expenses-tracker-data.json";
+const FILE_PATH = path.join(FOLDER_NAME, FILE_NAME);
+
+// config action
+const ACTIONS = { add: "insert", edit: "update", del: "delete" };
+
+export { FOLDER_NAME, FILE_NAME, FILE_PATH, ACTIONS };
